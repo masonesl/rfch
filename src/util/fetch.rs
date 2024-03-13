4c2105bf8960e9f83b75b33920fa3f7111b1f901
Mason <esslinger.mason@proton.me>
2024-03-13 16:09:36 -0600

    clean up code to reduce nesting

--- src/util/fetch.rs
+++ src/util/fetch.rs
@@ -9,73 +9,56 @@ pub mod fetch {
     }
 
     pub fn username() -> String {
-        match env::var("USER") {
-            Ok(name) => name,
-            Err(_) => unknown!(),
-        }
+        return env::var("USER")
+            .unwrap_or(unknown!());
     }
 
     pub fn hostname() -> String {
-        match System::host_name() {
-            Some(name) => name,
-            None => unknown!(),
-        }
+        return System::host_name()
+            .unwrap_or(unknown!());
     }
 
     pub fn osname() -> String {
-        match System::name() {
-            Some(name) => name,
-            None => unknown!(),
-        }
+        return System::name()
+            .unwrap_or(unknown!());
     }
 
     pub fn kernel() -> String {
-        match System::kernel_version() {
-            Some(name) => name,
-            None => unknown!(),
-        }
+        return System::kernel_version()
+            .unwrap_or(unknown!());
     }
 
     pub fn editor() -> String {
-        match env::var("EDITOR") {
-            Ok(name) => name,
-            Err(_) => unknown!(),
-        }
+        return env::var("EDITOR")
+            .unwrap_or(unknown!());
     }
 
     pub fn terminal() -> String {
-        match env::var("TERM") {
-            Ok(name) => name,
-            Err(_) => unknown!(),
-        }
+        return env::var("TERM")
+            .unwrap_or(unknown!());
     }
 
     pub fn desktop() -> String {
-        match env::var("XDG_CURRENT_DESKTOP") {
-            Ok(name) => name,
-            Err(_) => {
-                match env::var("DESKTOP_SESSION") {
-                    Ok(name) => name,
-                    Err(_) => unknown!(),
-                }
-            }
-        }
+        return env::var("XDG_CURRENT_DESKTOP")
+            .unwrap_or_else(|_| env::var("DESKTOP_SESSION")
+                            .unwrap_or(unknown!()));
     }
 
     pub fn shell() -> String {
         // attempt to read the SHELL environment variable
-        match env::var("SHELL") {
+        return match env::var("SHELL") {
             Ok(shell_path) => {
                 // convert the shell binary path to Path object and grab the actual binary name
                 match path::Path::new(&shell_path).file_name() {
-                    Some(shell_name) => match shell_name.to_str() {
-                        Some(shell_name_str) => shell_name_str.to_string(),
-                        None => unknown!()
+                    Some(shell_name) => {
+                        shell_name.to_str()
+                            .unwrap_or("unknown")
+                            .to_string()
                     },
                     None => unknown!(),
                 }
             },
             Err(_) => unknown!(),
-        }
+        };
     }
 }
